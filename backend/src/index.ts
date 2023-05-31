88739fb24f71576b3e15c3c63f536ada0c0d4d90
Cecilia Wahome <cece@Cecilias-MacBook-Pro.local>
2023-05-31 11:39:05 +0300

    express server folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
 import express, {Express} from "express"
 import cors from "cors"
-import dotenv from "dotenv"
+
 import { errorHandler } from "./middlewares/errorHandler"
 import todoRoutes from "./routes/todoRoutes"
 import deletedTodoRoutes from "./routes/deletedTodoRoutes"
 
-dotenv.config()
+
 
 const app: Express = express()
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import app from "./index"
+import dotenv from "dotenv"
+dotenv.config()
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT
 
 
 app.listen(PORT, ()=> {
